d7243ef025929c59712442e84dbd710ad0917e07
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2020-11-30 17:10:55 +0100

    Update PennElement_eyetracker.js

--- js_includes/PennElement_eyetracker.js
+++ js_includes/PennElement_eyetracker.js
@@ -128,7 +128,6 @@ window.PennController._AddElementType("EyeTracker", function(PennEngine) {
                 position: 'absolute', top: 'calc(0.5*97vh - 0.0125*97vw)', bottom: 'calc(0.4875*97vw)', width: "2.5vw", height: "2.5vw",  color: "red", 'background-color': 'pink'
             }).click(function(){
                 calibrationDiv.find('button').remove();
-                calibrationDiv.append($("<button>Look here!</button>").attr('disabled', true);
                 // Launches calculation per se
 			    $(this).attr('disabled', true);
                 storePoints = true;
