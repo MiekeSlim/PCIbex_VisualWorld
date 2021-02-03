bee2b415ae6cc31c4b36d791b5929d0b09f145dc
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2021-02-03 18:21:54 +0100

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -7,20 +7,7 @@ AddHost("https://users.ugent.be/~mslim/VW_DWR_Stimuli/images/");
 // PHP script that receives, stores (and will also output) the eye-tracking data
 EyeTrackerURL("https://users.ugent.be/~mslim/PCIbexData/EyeTracker.php")
 
-//Sequence("Welcome1","Welcome2", "CheckPreload", "AudioSetUp", "AudioCheck", "WebcamSetUp", "CalibrationSetUp", "Instructions", "Experiment", "QuestionnairePage", "Send",  "Final")
-/*
-PennController("Consent",
-    newText("ConsentText", "<p>This experiment has been approved by the Ethical Comittee from the Faculty of Psychology and Educational Sciences at Ghent University. We request your consent for participation in this experiment. Therefore, please read the following carefully: </p > <p>I declare that I, as a participant in a research project in the Department of Experimental Psychology at Ghent University:<br><br> <ol> <li> have been informed about the research objectives, the questions and the tasks that I will encounter during the research and that I was given the opportunity to receive further information if desired<br><br> </li><li> will participate out of free will in the research project <br><br> </li><li> am aware that the researchers do not collect any personal information that may be used to identify my identity (such as video recordings). All the data that will be collected is completely anonymized; <br><br> </li><li> give informed consent to the researchers to store, process, and report my data in anonymized form <br><br> </li><li> am aware of the option to stop my participation in this research at any moment in time without having to provide a reason; <br><br> </li><li> know that participating or stopping my participation in the research has no negative consequences of any kind for me <br><br> </li><li> am aware of the option to ask the researcher(s) for a summary of the results after the study is finished and the results have been known; <br><br> </li><li> agree that my data may be used for further analysis by other researchers after complete anonymization; <br><br> </li><li> am aware that Ghent University is the responsible entity with regards to the personal information collected during the study. I am also aware that the data protection officer can give me more information about the protection of my personal information. Contact: Hanne Elsen (privacy@ugent.be).</li> </ol> <br>In case you give your informed consent to participate in this study, please click on the button below. If you do not give your informed consent, please close this experiment. </p>")
-    ,
-    newCanvas( "myCanvas", 700 , 750)
-        .settings.add(0,0, getText("ConsentText"))
-        .print()         
-    ,
-    newButton("I have read the study information and give my informed consent. Continue to the next page")
-            .center()
-            .print()
-            .wait()
-)
+Sequence( "Welcome1", "Welcome2", "CheckPreload", "AudioSetUp", "AudioCheck", "WebcamSetUp", "CalibrationSetUp", "Instructions", randomize("Experiment"), "QuestionnairePage", "Send",  "Final")
 
 // Welcome page 1
 PennController("Welcome1",
@@ -30,18 +17,11 @@ PennController("Welcome1",
         .settings.add(0,0, getText("WelcomeText"))
         .print()         
     ,
-    newTextInput("Subject", randomnumber = Math.floor(Math.random()*1000000))    
-    ,
-    newVar("Subject")
-        .settings.global()
-        .set( getTextInput("Subject") )
-    ,           
     newButton("Take me to the next page")
         .center()
         .print()
         .wait()
 )
-    .log( "Subject" , getVar("Subject") )
 
 // Welcome page 2
 PennController("Welcome2",
@@ -56,10 +36,10 @@ PennController("Welcome2",
         .print()
         .wait()
 )
-*/
-// Wait if the resources have not finished preloading by now
+
+// Wait if the resources have not finished preloading by the time the tracker is calibrated
 CheckPreloaded("CheckPreload")
-               
+
 // Audio set-up
 PennController("AudioSetUp",
     newText("AudioInstructions", "<p>Here, you can play one of the sentences that will be used in the experiment. Please use this audio recording to adjust your volume. Feel free to replay this sentence as often as you need.</p>")
@@ -121,8 +101,9 @@ newTrial("WebcamSetUp",
     ,
     newButton("Take me to the next page (which will appear in fullscreen)")
         .center()
-        .print()        
-        .wait( newEyeTracker("tracker").test.ready() ) 
+        .print()
+        .wait()
+        .wait( newEyeTracker("tracker").test.ready() )
     ,
     fullscreen()
 )
@@ -141,13 +122,13 @@ newTrial("CalibrationSetUp",
         .wait( newEyeTracker("tracker").test.ready() )
         .remove()
     ,
-    getEyeTracker("tracker").calibrate(60)
+    getEyeTracker("tracker").calibrate(5)
         .log()
 )
 
 // Experiment instructions
 newTrial("Instructions", 
-    newText("TaskInstructions", "<p>You're all set to start the experiment! First, two trials will follow in which you won't hear anything, but see a picture appear in one of the four corners of the screen. Please look at this picture. <br> <br> After these two trials, the experimental trials will begin. In these experimental trials, the task is very simple: You will hear a couple of short sentences while you look at four pictures shown on the screen. The webcam will follow your eye movements during this task. <br><br> After the experimental trials, two trials will be shown again in which you will see one picture appear in one of the corners of the screen. Again, just look at this picture untill the next trial begins! <br> <br> Before each trial, you will see a button in the middle of your screen. Click on this button and look at it for three seconds. The webcam will check whether it is still calibrated. If it is, the trial will automatically start after three seconds. Otherwise, the calibration procedure will be repeated. <br><br>  During the trials, you don't need to click on anything: Just listen and watch!</p>")
+    newText("TaskInstructions", "<p>You're all set to start the experiment! The task in this experiment is very simple: You will hear a couple of short sentences while you look at the screen. The webcam will follow your eye movements during this task. <br> <br>Before each trial, you will see a button in the middle of your screen. Click on this button and look at it for three seconds. The webcam will check whether it is still calibrated. If it is, the trial will automatically start after three seconds. Otherwise, the calibration procedure will be repeated. <br><br>  During the trials, you don't need to click on anything: Just listen and watch!</p>")
     ,
     newCanvas("myCanvas", 800 , 300)
         .settings.add(0,0, getText("TaskInstructions"))
@@ -159,51 +140,76 @@ newTrial("Instructions",
         .wait()
 )
 
-Template("ListA.csv", row =>
-    newTrial(
-        newEyeTracker("tracker").calibrate(60)  // Make sure that the tracker is still calibrated
+// Only run 2 first trials defined in the table
+Template("trials.csv", row =>
+    newTrial("Experiment",
+        //show cursor     
+        newFunction( ()=>{
+            $("body").css({
+                width: '100vw',
+                height: '100vh',
+                cursor: 'default'
+           });
+        }).call()
+        ,    
+        newEyeTracker("tracker").calibrate(5)  // Make sure that the tracker is still calibrated
         ,
-        newTimer(250).start().wait()
+        defaultImage.size("20vh", "20vh")
         ,
-        defaultImage.size("20vh", "20vh"),
         // We print the four images at the four corners
-        newCanvas("topFemaleIA", "40vw", "40vh")  // The Canvas are bigger than the images they contain
-            .add( "center at 25%" , "middle at 50%" , newImage(row.Image1) )
+        newCanvas("target_image", "40vw", "40vh")  // The Canvas are bigger than the images they contain
+            .add( "center at 50%" , "middle at 50%" , newImage(row.target_image) )
             .print( "center at 25vw" , "middle at 25vh" )
         ,
-        newCanvas("bottomFemaleIA", "40vw", "40vh")
-            .add( "center at 25%" , "middle at 50%" , newImage(row.Image2) )
+        newCanvas("distractor_image1", "40vw", "40vh")
+            .add( "center at 50%" , "middle at 50%" , newImage(row.distractor_image1) )
             .print( "center at 25vw" , "middle at 75vh" )
         ,
-        newCanvas("topMaleIA", "40vw", "40vh")
-            .add( "center at 25%" , "middle at 50%" , newImage(row.Image3) )
+        newCanvas("distractor_image2", "40vw", "40vh")
+            .add( "center at 50%" , "middle at 50%" , newImage(row.distractor_image2) )
             .print( "center at 75vw" , "middle at 25vh" )
         ,
-        newCanvas("bottomMaleIA", "40vw", "40vh")
-            .add( "center at 25%" , "middle at 50%" , newImage(row.Image4) )
+        newCanvas("distractor_image3", "40vw", "40vh")
+            .add( "center at 50%" , "middle at 50%" , newImage(row.distractor_image3) )
             .print( "center at 75vw" , "middle at 75vh" )
-        ,
-        newAudio(row.context_soundfile).play().wait()
-        ,
-        getImage("backTF").remove(),getImage("backBF").remove(),getImage("backTM").remove(),getImage("backBM").remove()
         ,
         getEyeTracker("tracker")
             // We track the Canvas: making them bigger allows us to capture look-estimates slightly off the images themselves
-            .add( getCanvas("topFemaleIA") , getCanvas("bottomFemaleIA") , getCanvas("topMaleIA") , getCanvas("bottomMaleIA") )
+            .add( getCanvas("target_image") , getCanvas("distractor_image1") , getCanvas("distractor_image2") , getCanvas("distractor_image3") )
             .start()
-            .log()  // IMPORTANT: if you don't log, the eye-tracking data will NOT get sent
+            .log()  // IMPORTANT: if you don't log, the eye-tracking data will NOT get sent        
+        ,   
+        // Hide the mouse cursor
+        newFunction( ()=>{
+            $("body").css({
+                width: '100vw',
+                height: '100vh',
+                cursor: 'none'
+           });
+        }).call()
+        ,    
+        newSelector("Selector") //The only purpose of this selector is to randomise the positioning of the images on the screen
+            .add(getCanvas("target_image") , getCanvas("distractor_image1") , getCanvas("distractor_image2") , getCanvas("distractor_image3"))
+            .shuffle()             
+            .remove()
         ,
-        newTimer(500).start().wait()
+        newTimer(2200).start().wait()
         ,
-        newAudio("test", row.Audio).log().play()
-        ,
-        newTimer(250).start().wait()
-        ,
+        newAudio("test", row.english_audio)
+            .log()
+            .play()
+            .wait()
+         ,
         getEyeTracker("tracker").stop() // Stop now to prevent collecting unnecessary data
-
-    )
+        )
+    .log( "target_image"        , row.target_image          )
+    .log( "distractor_image1"   , row.distractor_image1     )            
+    .log( "distractor_image2"   , row.distractor_image2          )   
+    .log( "distractor_image3"   , row.distractor_image3          )            
+    .log( "sentence"            , row.english_audio          )           
+    .log( "type"                , row.StimulusType          )  
+    .log( "condition"           , row.StimulusCondition       )       
 )
-
 
 PennController.SendResults("Send");
 
@@ -215,3 +221,4 @@ newTrial("Final",
     newButton("waitforever").wait() // Not printed: wait on this page forever
 )
 .setOption("countsForProgressBar",false)
+
